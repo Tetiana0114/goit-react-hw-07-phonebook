6d4fd8da0db4f8d1d7c2cf1f6cae5f60c59d967a
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-11-25 15:38:34 +0200

    Delete persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { rootReducer } from './reducer';
 
-const persistConfig = {
-  whitelist: ['contacts'],
-  key: 'root',
-  storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
